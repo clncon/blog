e167b2183bba4639b4089cdd31b3944a25bc91ed
kongmin <clncon@163.com>
2020-01-04 15:42:25 +0800

    fix hook.sh

--- hook.sh
+++ hook.sh
@@ -5,5 +5,5 @@ echo "----------install update!!!-------------------"
 go install
 echo "----------restart blog!!!---------------------"
 ps -ef | grep -v grep | grep blog  | awk '{print $2}' | xargs kill -9
-/root/go/bin/blog.sh
+nohup /root/go/bin/blog.sh > /root/go/bin/run.log 2>&1 &
 echo "----------auto deploy complate!!!!---------------------"
