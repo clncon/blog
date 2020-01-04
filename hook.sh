c34e722a8a3ab9140f3dfa18a282886f2828eeed
clncon <clncon@163.com>
2020-01-04 16:02:01 +0800

    fix hook.sh

--- hook.sh
+++ hook.sh
@@ -4,6 +4,6 @@ git pull
 echo "----------install update!!!-------------------"
 go install
 echo "----------restart blog!!!---------------------"
-ps -ef | grep -v grep | grep blog  | awk '{print $2}' | xargs kill -9
-nohup /root/go/bin/blog.sh > /root/go/bin/run.log 2>&1 &
+ps -ef | grep -v grep | grep /root/go/bin/blog  | awk '{print $2}' | xargs kill -9
+nohup /root/go/bin/blog > /root/go/bin/run.log 2>&1 &
 echo "----------auto deploy complate!!!!---------------------"
