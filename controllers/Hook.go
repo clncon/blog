40d4e97b15b9f9607a4048863bb9843b686dcf7d
kongmin <clncon@163.com>
2020-01-04 17:03:55 +0800

    add hook signature

--- controllers/Hook.go
+++ controllers/Hook.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"blog/system"
+	"crypto/hmac"
+	"crypto/sha1"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"os/exec"
@@ -10,9 +12,17 @@ import (
 func Go(c *gin.Context) {
 	//获取可执行文件路径hook.sh
 	command := system.GetConfiguration().ShellPath
+	serect:="135696cc92c1d9c0a74e956a4594652b"
+	buff:=make([]byte,1024)
+	n,_:=c.Request.Body.Read(buff)
+	mac:=hmac.New(sha1.New,[]byte(serect))
+
+	mac.Write(buff[0:n])
+    result:=string(mac.Sum(nil))
+
     signature :=c.GetHeader("X-Hub-Signature")
 	doShell(command)
-	c.JSON(200, signature)
+	c.JSON(200, signature+":"+result)
 }
 
 func doShell(command string) {
